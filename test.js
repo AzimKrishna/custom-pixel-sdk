b45fc4431573165b64857d9e2c147436384c24b6
Azim Baji Krishna <azimbaji19@gmail.com>
2024-06-03 15:29:44 +0530

    Logic inside third party js file

--- test.js
+++ test.js
@@ -1,77 +1,74 @@
-(function (window, document) {
-  var mpq = (window.mpq = window.mpq || []);
+(function(window, document) {
+  var mpq = window.mpq = window.mpq || [];
 
-  mpq.callMethod = function () {
+  mpq.callMethod = function() {
     mpq.push(arguments);
   };
 
-  mpq.init = function (pixelId, options) {
+  mpq.init = function(pixelId) {
     mpq.pixelId = pixelId;
-    mpq.debug = options && options.debug;
     mpq.queue = [];
-    mpq.batchInterval = (options && options.batchInterval) || 10000;
+    mpq.endpoint = "https://custom-pixel-sdk.onrender.com/pixel";
 
-    if (mpq.debug) {
-      console.log("Custom pixel initialized with ID:", pixelId);
+    function sendEvent(eventName, eventData) {
+      var payload = {
+        pixelId: mpq.pixelId,
+        event_name: eventName,
+        event_data: eventData,
+        url: window.location.href,
+        referrer: document.referrer,
+        timestamp: new Date().toISOString(),
+      };
+
+      fetch(mpq.endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+        keepalive: true,
+      }).catch(console.error);
     }
 
-    if (options && options.batch) {
-      setInterval(function () {
-        if (mpq.queue.length > 0) {
-          var batch = mpq.queue.splice(0, mpq.queue.length);
-          mpq.sendBatch(batch);
-        }
-      }, mpq.batchInterval);
-    }
-  };
+    analytics.subscribe('page_viewed', function(event) {
+      sendEvent('PageView', { pageEventId: event.id, timeStamp: event.timestamp });
+    });
 
-  mpq.track = function (event, data) {
-    if (!mpq.pixelId) {
-      console.error("Pixel not initialized. Call mpq.init() first.");
-      return;
-    }
-    var payload = {
-      pixelId: mpq.pixelId,
-      event: event,
-      data: data,
-      url: window.location.href,
-      referrer: document.referrer,
-      timestamp: new Date().toISOString(),
-    };
+    analytics.subscribe('product_viewed', function(event) {
+      sendEvent('ViewContent', {
+        content_ids: [event.data?.productVariant?.id],
+        content_name: event.data?.productVariant?.title,
+        currency: event.data?.productVariant?.price.currencyCode,
+        value: event.data?.productVariant?.price.amount,
+      });
+    });
 
-    if (mpq.debug) {
-      console.log("Event tracked:", payload);
-    }
+    analytics.subscribe('search_submitted', function(event) {
+      sendEvent('Search', { search_string: event.searchResult.query });
+    });
 
-    if (mpq.batch) {
-      mpq.queue.push(payload);
-    } else {
-      mpq.sendData(payload);
-    }
-  };
+    analytics.subscribe('product_added_to_cart', function(event) {
+      sendEvent('AddToCart', {
+        content_ids: [event.data?.cartLine?.merchandise?.productVariant?.id],
+        content_name: event.data?.cartLine?.merchandise?.productVariant?.title,
+        currency: event.data?.cartLine?.merchandise?.productVariant?.price?.currencyCode,
+        value: event.data?.cartLine?.merchandise?.productVariant?.price.amount,
+      });
+    });
 
-  mpq.sendData = function (payload) {
-    try {
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://custom-pixel-sdk.onrender.com/pixel", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(JSON.stringify(payload));
-    } catch (error) {
-      console.error("Error sending pixel data:", error);
-    }
-  };
+    analytics.subscribe('payment_info_submitted', function(event) {
+      sendEvent('AddPaymentInfo', {});
+    });
 
-  mpq.sendBatch = function (batch) {
-    try {
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://custom-pixel-sdk.onrender.com/pixel", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(JSON.stringify(batch));
-      if (mpq.debug) {
-        console.log("Batch sent:", batch);
-      }
-    } catch (error) {
-      console.error("Error sending batch pixel data:", error);
-    }
+    analytics.subscribe('checkout_started', function(event) {
+      sendEvent('InitiateCheckout', {});
+    });
+
+    analytics.subscribe('checkout_completed', function(event) {
+      sendEvent('Purchase', {
+        currency: event.data?.checkout?.currencyCode,
+        value: event.data?.checkout?.totalPrice?.amount,
+      });
+    });
   };
 })(window, document);
