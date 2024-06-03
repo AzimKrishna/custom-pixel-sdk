5aa37a361bb08533d11c6a991a284bc52ef08884
Azim Baji Krishna <azimbaji19@gmail.com>
2024-06-03 11:52:20 +0530

    inial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors()); // Add CORS support
+
+app.post('/pixel', (req, res) => {
+  const event = req.body;
+  console.log('Event received:', event);
+  // Store the event data in your database
+  res.status(200).send('Event received');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World! backend working');
+});w
+
+app.listen(3000, () => {
+  console.log('Pixel server listening on port 3000');
+});
